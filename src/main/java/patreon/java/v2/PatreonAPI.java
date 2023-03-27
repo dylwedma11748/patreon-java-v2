6d8424d9effe63dc8223c059c246263b2761cb54
Dylan Wedman <32218999+dylwedma11748@users.noreply.github.com>
2023-03-27 13:12:30 -0500

    Added new fetchers
    Added some new fetchers for some of the Resource classes.

--- src/main/java/patreon/java/v2/PatreonAPI.java
+++ src/main/java/patreon/java/v2/PatreonAPI.java
@@ -1,14 +1,10 @@
 package patreon.java.v2;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
-
-import javax.print.attribute.standard.Media;
 
 import org.apache.http.client.utils.URIBuilder;
 import org.slf4j.Logger;
@@ -16,18 +12,20 @@ import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.github.jasminb.jsonapi.DeserializationFeature;
 import com.github.jasminb.jsonapi.JSONAPIDocument;
 import com.github.jasminb.jsonapi.ResourceConverter;
+import com.github.jasminb.jsonapi.SerializationFeature;
 
 import patreon.java.v2.resources.Address;
 import patreon.java.v2.resources.Benefit;
 import patreon.java.v2.resources.Campaign;
-import patreon.java.v2.resources.Campaign;
+import patreon.java.v2.resources.Campaign.CampaignField;
 import patreon.java.v2.resources.Deliverable;
 import patreon.java.v2.resources.Goal;
+import patreon.java.v2.resources.Media;
 import patreon.java.v2.resources.Member;
+import patreon.java.v2.resources.Member.MemberField;
 import patreon.java.v2.resources.PledgeEvent;
 import patreon.java.v2.resources.Post;
 import patreon.java.v2.resources.RequestUtil;
@@ -38,6 +36,16 @@ import patreon.java.v2.resources.shared.BaseResource;
 import patreon.java.v2.resources.shared.Field;
 
 public class PatreonAPI {
+	
+	class Resolver implements com.github.jasminb.jsonapi.RelationshipResolver  {
+		@Override
+		public byte[] resolve(String relationshipURL) {
+			System.out.println(relationshipURL);
+			return null;
+		}
+		
+	}
+	
 	/**
 	 * The base URI for requests to the patreon API. This may be overridden (e.g.
 	 * for testing) by passing -Dpatreon.rest.uri="https://my.other.server.com" as
@@ -72,27 +80,40 @@ public class PatreonAPI {
 		this.converter = new ResourceConverter(objectMapper, Address.class, Benefit.class, Campaign.class, Deliverable.class, Goal.class, Media.class, Member.class, PledgeEvent.class, Post.class, Tier.class, User.class);
 		
 		this.converter.enableDeserializationOption(DeserializationFeature.ALLOW_UNKNOWN_INCLUSIONS);
+		this.converter.enableSerializationOption(SerializationFeature.INCLUDE_RELATIONSHIP_ATTRIBUTES);
+		this.converter.setGlobalResolver(new Resolver());
 	}
 
 	public JSONAPIDocument<User> fetchUser() throws IOException {
-		URIBuilder pathBuilder = new URIBuilder().setPath("identity").addParameter("include", "memberships");
+		URIBuilder pathBuilder = new URIBuilder().setPath("identity").addParameter("include", "memberships,campaign");
 		
 		Collection<UserField> userFields = User.UserField.getAllFields();
-		//Collection<CampaignField> campaignFields = Campaign.CampaignField.getAllFields();
 		addFieldsParam(pathBuilder, User.class, userFields);
 		
-		InputStream response = getDataStream(pathBuilder.toString());
-		InputStreamReader reader = new InputStreamReader(response);
-		BufferedReader breader = new BufferedReader(reader);
+		return converter.readDocument(getDataStream(pathBuilder.toString()), User.class);
+	}
+	
+	public JSONAPIDocument<User> fetchUser(Collection<User.UserField> userFields) throws IOException {
+		URIBuilder pathBuilder = new URIBuilder().setPath("identity");
+		addFieldsParam(pathBuilder, User.class, userFields);
 		
-		String line;
-		
-		while ((line = breader.readLine()) != null) {
-			System.out.println(line);
-		}
-		
-		return null;
-		//return converter.readDocument(getDataStream(pathBuilder.toString()), User.class);
+		return converter.readDocument(getDataStream(pathBuilder.toString()), User.class);
+	}
+	
+	public JSONAPIDocument<List<Campaign>> fetchCampaigns() throws IOException {
+		URIBuilder pathBuilder = new URIBuilder().setPath("campaigns").addParameter("include", "tiers,creator,benefits,goals");
+		Collection<CampaignField> campaignFields = Campaign.CampaignField.getAllFields();
+		addFieldsParam(pathBuilder, Campaign.class, campaignFields);
+		System.out.println(pathBuilder.toString());
+		return converter.readDocumentCollection(getDataStream(pathBuilder.toString()), Campaign.class);
+	}
+	
+	public JSONAPIDocument<List<Member>> fetchMembers(String campaignID) throws IOException  {
+		URIBuilder pathBuilder = new URIBuilder().setPath("campaigns/" + campaignID + "/members").addParameter("include", "address,campaign,currently_entitled_tiers,user");
+		Collection<MemberField> memberFields = Member.MemberField.getAllFields();
+		addFieldsParam(pathBuilder, Member.class, memberFields);
+		System.out.println(pathBuilder.toString());
+		return converter.readDocumentCollection(getDataStream(pathBuilder.toString()), Member.class);
 	}
 	
 	private InputStream getDataStream(String suffix) throws IOException {
