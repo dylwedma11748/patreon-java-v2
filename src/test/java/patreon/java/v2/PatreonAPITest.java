fd091634550d838c92c3b9eb5ee87b6e709fd61f
Dylan Wedman <32218999+dylwedma11748@users.noreply.github.com>
2023-03-28 17:48:31 -0500

    Update PatreonAPITest.java

--- src/test/java/patreon/java/v2/PatreonAPITest.java
+++ src/test/java/patreon/java/v2/PatreonAPITest.java
@@ -7,21 +7,79 @@ import com.github.jasminb.jsonapi.JSONAPIDocument;
 
 import patreon.java.v2.resources.Campaign;
 import patreon.java.v2.resources.Member;
+import patreon.java.v2.resources.PledgeEvent;
+import patreon.java.v2.resources.Tier;
 import patreon.java.v2.resources.User;
 import patreon.java.v2.resources.shared.PaginationMeta;
 
 public class PatreonAPITest {
 
 	private static final String PATREON_ACCESS_KEY = "YOUR_ACCESS_KEY";
+	private static final PatreonAPI apiClient = new PatreonAPI(PATREON_ACCESS_KEY);
 	
 	public static void main(String[] args) {
-		PatreonAPI apiClient = new PatreonAPI(PATREON_ACCESS_KEY);
+		testUser();
+		testCampaign();
+	}
+	
+	private static void testMember() {
+		JSONAPIDocument<Member> memberResponse = null;
+		
+		try {
+			memberResponse = apiClient.fetchMember("CAMPAIGN_ID");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		Member member = memberResponse.get();
+		System.out.println(member.getCampaign().getCreatedAt());
+	}
+	
+	private static void testUser() {
+		JSONAPIDocument<User> userResponse = null;
+		
+		try {
+			userResponse = apiClient.fetchUser();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		User user = userResponse.get();
+		Campaign campaign = user.getCampaign();
+		
+		System.out.println(campaign.getCreatedAt());
+		
+	}
+	
+	private static void testCampaign() {
+		JSONAPIDocument<List<Campaign>> campaginResponse = null;
+		
+		try {
+			campaginResponse = apiClient.fetchCampaigns();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		List<Campaign> campagins = campaginResponse.get();
+		
+		for (Campaign campaign : campagins) {
+			List<Tier> tiers = campaign.getTiers();
+			
+			for (Tier tier : tiers) {
+				System.out.println(tier.getTitle());
+			}
+			
+			testMember(campaign.getId());
+		}
+	}
+	
+	private static void testMember(String id) {
 		JSONAPIDocument<List<Member>> memberResponse = null;
 		
 		try {
-			memberResponse = apiClient.fetchMembers("CAMPAIGN_ID");
+			memberResponse = apiClient.fetchMembers(id);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
@@ -31,7 +89,22 @@ public class PatreonAPITest {
 		System.out.println(meta.getTotal());
 		
 		for (Member member : members) {
-			System.out.println(member.full_name);
+			Campaign c = member.getCampaign();
+			System.out.println("Campaign Date: " + c.getPublishedAt());
+			
+			System.out.println("Member patron status: " + member.getPatronStatus());
+			
+			List<Tier> tiers = member.getCurrentlyEntitledTiers();
+			
+			for (Tier tier : tiers) {
+				System.out.println(tier.getAmountCents());
+			}
+			
+			List<PledgeEvent> pledgeHistory = member.getPledgeHistory();
+			
+			for (PledgeEvent event : pledgeHistory) {
+				System.out.println("Pledge event tier: " + event.getTier());
+			}
 		}
 	}
 
